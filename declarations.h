49b572ed7cd00731c4660bded3d0cedad8b3faf0
Eric Falheim <ericfalheim@hotmail.com>
2015-10-06 01:31:44 +0200

    Update declarations.h

--- declarations.h
+++ declarations.h
@@ -19,8 +19,8 @@ typedef struct action
 {
   int type; // NOTHING = 0, ADD = 1, REMOVE = 2, EDIT = 3
   int index; // index for listing goods etc. in DB
-  int s_index; // used for storing index when edit/ remove, even thou user may use index for other things.
-  Good copy; 
+  int s_index; // used for storing index when edit/ remove, even thou user may use list.
+  Good copy; // copy of good 
 }action_t;
 
 //FRAMEWORK
@@ -28,14 +28,13 @@ void print_main_menu(void);
 void add_action(DB_t *d, action_t *a);
 void remove_action(DB_t *d, action_t *a);
 void edit_action(DB_t *d, action_t *a);
-int edit_good(Good *g);      //return cancel int
-int listing_action(DB_t *d, int page, action_t *a); // return cancel int
+int edit_good(Good *g);
+int listing_action(DB_t *d, int page, action_t *a);
 void undo_action(DB_t *d, action_t *a);
 bool quit_action(void);
-
 void list_db(DB_t *p, int n);
-
 void print_good(Good g);
+
 
 //INPUT
 char *get_string_input(void);
@@ -45,7 +44,7 @@ char ask_question_char(char *c, char *d);
 
 
 //DB
-void add_to_db(DB_t *head, Good g); // add to end
+void add_to_db(DB_t *head, Good g);
 int len_db(DB_t *head);
 Good get_good_db_n(DB_t *h, int n);
 void replace_n_db(DB_t *h, Good g, int i);
@@ -56,4 +55,6 @@ void insert_at_n(DB_t *h, Good g, int i);
 // MISC
 void create_init_db(DB_t *head);
 void clear_screen(void);
+int row_to_int(char c);
+int invalid_col(int n);
 void free_memory(DB_t *db, action_t *a);
